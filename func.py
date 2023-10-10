2f67f9e9b456fbcbc47ad6d8c3c2265104055ce2
somethingstrang3 <146678680+somethingstrang3@users.noreply.github.com>
2023-10-10 11:53:31 +0300

    2.

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -10,11 +11,34 @@ class TicTac(QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
 
 
+        for i in range(9):
+            self.ui.buttons[i].clicked.connect(self.process)
+
+
+
+
+
+
+
+    def process(self):
+        self.sender().setText(self.ui.switcher.button(self.ui.switcher.checkedId()).text())
+        self.sender().setEnabled(False)
+
+        self.ui.switcher.button(0).setEnabled(False)
+        self.ui.switcher.button(1).setEnabled(False)
+        if self.ui.switcher.checkedId() == 0:
+            self.ui.status_label.setText("ход игрока X")
+            self.ui.switcher.button(1).setChecked(True)
+        else:
+            self.ui.status_label.setText("ход игрока 0")
+            self.ui.switcher.button(0).setChecked(True)
+
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = TicTac()
     window.show()
-
     sys.exit(app.exec_())
